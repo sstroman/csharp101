79614e95afb5b56a30cfda672b7ac34e14e6ad77
sstroman <stephanie.stroman@gmail.com>
2014-07-30 15:16:59 -0400

    added fizzbuzz

--- Basics/LogicOperators.cs
+++ Basics/LogicOperators.cs
@@ -10,5 +10,27 @@ namespace Basics {
       Assert.AreEqual(false, false && true);
       Assert.AreEqual(true, true && true);
     }
+
+    [Test]
+    public void FizzBuzzer() {
+      Assert.AreEqual("Fizz", FizzBuzz(3));
+      Assert.AreEqual("Buzz", FizzBuzz(5));
+      Assert.AreEqual("FizzBuzz", FizzBuzz(30));
+    }
+
+    private string FizzBuzz(int number) {
+      bool divisibleBy3 = (number % 3) == 0;
+      bool divisibleBy5 = (number % 5) == 0;
+      string result = "";
+
+      if (divisibleBy3) {
+        result+= "Fizz";
+      }
+      if (divisibleBy5) {
+        result+= "Buzz";
+      }
+
+      return result;
+    }
   }
 }
